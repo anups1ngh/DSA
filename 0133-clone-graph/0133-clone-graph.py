09229ed42323de728fc96852bc60bc6c4757646f
Anup Kumar Singh <86849540+anups1ngh@users.noreply.github.com>
2023-08-09 20:19:45 +0530

    Time: 44 ms (85.91%), Space: 17.1 MB (28.07%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -5,20 +5,8 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
+import copy
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        old_to_new = {}
-        
-        def clone(node):
-            if node in old_to_new:
-                return old_to_new[node]
-            
-            copy = Node(node.val)
-            old_to_new[node] = copy
-            
-            for nei in node.neighbors:
-                copy.neighbors.append(clone(nei))
-            return copy 
-        
-        return clone(node) if node else None
+        newNode = copy.deepcopy(node)
+        return newNode
