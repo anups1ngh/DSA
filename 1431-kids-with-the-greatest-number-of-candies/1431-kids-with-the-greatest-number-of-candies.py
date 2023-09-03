c057cf4e3a31efb3ae0db763dc9c7473899bd083
Anup Kumar Singh <86849540+anups1ngh@users.noreply.github.com>
2023-09-03 18:31:29 +0530

    Time: 53 ms (29.16%), Space: 16.2 MB (68.78%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -1,13 +1,4 @@
-class Solution(object):
-    def kidsWithCandies(self, candies, extraCandies):
-        max_candies = max(candies)
-        
-        result = []
-        
-        for candy in candies:
-            if candy + extraCandies >= max_candies:
-                result.append(True)
-            else:
-                result.append(False)
-        
-        return result
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        gauge = max(candies) - extraCandies 
+        return [candy >= gauge for candy in candies]
